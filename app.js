f4b17eb7d62732efcc565245adf4dc5da2d27a36
jahu5474 <jasper123ya@live.com>
2023-02-28 16:58:51 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+// tiktaktoe there is 2 player in the game
+const player1 = ();
+const player2 = ();
+
+
+//if you get 3 of the same symbol in a row you win
+
+//if not then it is a tie and the game restarts
+
